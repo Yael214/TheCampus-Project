67cad5c01633899c4bab4dafe3b0f609252ca927
Yael214 <yaeltamitai@gmail.com>
2026-04-28 18:01:10 +0300

    Initialize project with React, Vite and Tailwind 4

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import UserProfile from './components/UserProfile'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 py-10">
+      <UserProfile />
+    </div>
+  )
+}
+
+export default App
