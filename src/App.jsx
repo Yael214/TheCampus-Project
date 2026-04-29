0db5374cadd16bfab94aa1fbdc784e8bcf226ff6
Shanni <shani5000684@gmail.com>
2026-04-29 23:01:14 +0300

    cleanup: remove infrastructure check page before production

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import UserProfile from './components/UserProfile'
-import Shanicheck from "./Shanicheck";
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-100 py-10">
-      
-      <Shanicheck />
+      <UserProfile />
     </div>
   )
 }
